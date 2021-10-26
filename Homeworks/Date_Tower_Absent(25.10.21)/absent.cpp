3a79cb9854b84f1218ffcd69beef9d9c70d61700
Karkusha23 <min.rinat567321@gmail.com>
2021-10-26 19:53:42 +0300

    Add files via upload

---
+++ Homeworks/Date_Tower_Absent(25.10.21)/absent.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <time.h>
+
+void full(int m[], int n);
+
+int main()
+{
+    int n;
+    do
+    {
+        std::cout << "Input n: ";
+        std::cin >> n;
+    } while (n < 2);
+    int* m = new int[n];
+    full(m, n);
+    int s = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        s += m[i];
+        std::cout << m[i] << ' ';
+    }
+    delete[] m;
+    std::cout << std::endl << "Absent element: " << ((n * (n + 1)) / 2) - s << std::endl;
+}
+
+void full(int m[], int n)
+{
+    srand(time(NULL));
+    bool* ind = new bool[n + 1];
+    for (int i = 0; i < n + 1; ++i)
+    {
+        ind[i] = true;
+        if (i < n)
+        {
+            m[i] = -1;
+        }
+    }
+    int x;
+    for (int i = 0; i < n; ++i)
+    {
+        do
+        {
+            x = rand() % (n + 1);
+            if (ind[x])
+            {
+                m[i] = x;
+                ind[x] = false;
+            }
+        } while (m[i] == -1);
+    }
+    delete[] ind;
+}
