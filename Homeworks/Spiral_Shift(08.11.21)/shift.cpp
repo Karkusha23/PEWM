b8fb7f5f1847a6596df0ffda7e26be436b15be98
Karkusha23 <min.rinat567321@gmail.com>
2021-11-10 15:26:35 +0300

    Add files via upload

---
+++ Homeworks/Spiral_Shift(08.11.21)/shift.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+void shiftarray(int*, int, int);
+
+int main()
+{
+    int n;
+    do
+    {
+        std::cout << "Input size of array: ";
+        std::cin >> n;
+    } while (n < 1);
+    int* M = new int[n];
+    std::cout << "Input array: ";
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> M[i];
+    }
+    int k;
+    do
+    {
+        std::cout << "Input size of shift: ";
+        std::cin >> k;
+    } while (k < 1);
+    shiftarray(M, n, k);
+    std::cout << "After the shift: ";
+    for (int i = 0; i < n; ++i)
+    {
+        std::cout << M[i] << ' ';
+    }
+    delete[] M;
+}
+
+void shiftarray(int* M, int n, int k)
+{
+    int* A = new int[n];
+    for (int i = 0; i < n; ++i)
+    {
+        A[i] = M[(i + k) % n];
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        M[i] = A[i];
+    }
+    delete[] A;
+}
---
+++ Homeworks/Spiral_Shift(08.11.21)/spiral.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+
+void spiral(int**, int, int);
+void printarray(int**, int, int);
+
+int maxarray(int**, int, int);
+int nodigits(int);
+
+int main()
+{
+    int n;
+    do
+    {
+        std::cout << "Input number of rows: ";
+        std::cin >> n;
+    } while (n < 1);
+    int m;
+    do
+    {
+        std::cout << "Input number of columns: ";
+        std::cin >> m;
+    } while (m < 1);
+    int** M = new int*[n];
+    for (int i = 0; i < n; ++i)
+    {
+        M[i] = new int[m];
+    }
+    spiral(M, n, m);
+    printarray(M, n, m);
+    for (int i = 0; i < n; ++i)
+    {
+        delete[] M[i];
+    }
+    delete[] M;
+}
+
+void spiral(int** M, int n, int m)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            M[i][j] = 0;
+        }
+    }
+    int i = 0, j = 0, dir = 1;
+    bool col = true;
+    for (int k = 1; k <= n * m; ++k)
+    {
+        M[i][j] = k;
+        if (col)
+        {
+            if (j + dir > m - 1 || M[i][j + dir] != 0)
+            {
+                col = false;
+                i += dir;
+            }
+            else
+            {
+                j += dir;
+            }
+        }
+        else
+        {
+            if (i + dir > n - 1 || M[i + dir][j] != 0)
+            {
+                dir *= -1;
+                col = true;
+                j += dir;
+            }
+            else
+            {
+                i += dir;
+            }
+        }
+    }
+}
+
+void printarray(int** M, int n, int m)
+{
+    int h = nodigits(maxarray(M, n, m)) + 1;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cout << M[i][j];
+            for (int k = h - nodigits(M[i][j]); k > 0; --k)
+            {
+                std::cout.put(' ');
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+int maxarray(int** M, int n, int m)
+{
+    int h = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            h = M[i][j] > h ? M[i][j] : h;
+        }
+    }
+    return h;
+}
+
+int nodigits(int n)
+{
+    int k = 0;
+    while (n)
+    {
+        n /= 10;
+        ++k;
+    }
+    return k;
+}
